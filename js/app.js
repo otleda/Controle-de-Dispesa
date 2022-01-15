e560cbee6919b29b509a5fcfbd3b24eeb41dab62
Adelto Oliveira <adtguitar@gmail.com>
2022-01-15 01:32:29 -0300

    add recurso do metodo map(), reduce() para somar o total dos valores e toFixed() para add duas casas decimais

--- js/app.js
+++ js/app.js
@@ -1,29 +1,39 @@
 const transactionsUl = document.querySelector('#transactions')
 
 const dummyTransaction = [ 
-     {id: 1, name: 'corda de violao', amount: - 35},
-     {id: 2, name: 'salario', amount: 3.500},
-     {id: 3, name: 'freela', amount: 250},
-     {id: 4, name: 'mercado', amount: - 150},
+     {id: 1, name: 'Violao folk', amount: - 835},
+     {id: 2, name: 'Salario', amount: 3500},
+     {id: 3, name: 'Freela', amount: 250},
+     {id: 4, name: 'Mercado', amount: - 150},
 ];
 
-const addTransactionIntoDom = (transaction) => {
+const addTransactionIntoDom = (value) => {
 
-     const operator = transaction.amount < 0 ? '-' : '+'
-     const cssClass = transaction.amount < 0 ? 'minus' : 'plus'
-     const removeOperatoAmount = Math.abs(transaction.amount)
+     const operator = value.amount < 0 ? '-' : '+'
+     const cssClass = value.amount < 0 ? 'minus' : 'plus'
+
+     const removeOperatoAmount = Math.abs(value.amount)
+
      const li = document.createElement('li')
 
      li.classList.add(cssClass)
      li.innerHTML = `
-          ${transaction.name}<span>${operator} R$${removeOperatoAmount}</span><button class="delete-btn">x</button>
+          ${value.name}<span>${operator} R$${removeOperatoAmount}</span><button class="delete-btn">x</button>
      `
      transactionsUl.append(li)
 }
 
+/*Pegando valores da chave amount de dentro da array de obj dummyTransaction[],
+com map(), passando o reduce(), para somar todos os valores e passando toFIxed()
+para acrescentar duas casas decimais.
+*/
 const updateBalanceValues = () => {
-     const transactionsAmounts = dummyTransaction.map((value) => value.amount)
-     console.log(transactionsAmounts);
+     const valueAmounts = dummyTransaction.map(value => value.amount)
+
+     console.log(valueAmounts);
+     const total = valueAmounts.reduce((accumulator, value) => accumulator + value, 0).toFixed(2)
+
+     console.log(total);
 }
 
 const init = () => {
