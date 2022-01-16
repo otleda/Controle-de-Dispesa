8dc5eb4cab642bfe7301f6d4d18a9ffd27988122
Adelto Oliveira <adtguitar@gmail.com>
2022-01-16 19:51:30 -0300

    Criando evento para pegar os valores dos campos input e adicioando a array[]

--- js/app.js
+++ js/app.js
@@ -2,75 +2,105 @@ const transactionsEl = document.querySelector('#transactions')
 const currentValueDisplayEl = document.querySelector('#balance')
 const incomeDisplayEl = document.querySelector('#money-plus')
 const expenseDisplayEl = document.querySelector('#money-minus')
-
-console.log({currentValueDisplayEl, incomeDisplayEl, expenseDisplayEl})
+const formEl = document.querySelector('#form')
+const impuTextValueEL = document.querySelector('#text')
+const imputAmountValueEl = document.querySelector('#amount')
 
 const dummyTransaction = [ 
      {id: 1, name: 'Violao folk', amount: - 900},
      {id: 2, name: 'Salario', amount: 3500},
-     {id: 3, name: 'Freela', amount: 250},
+     {id: 3, name: 'Freela', amount: 250},   
      {id: 4, name: 'Jobs Design', amount: 150},
      {id: 5, name: 'Mercado', amount: - 150},
      {id: 6, name: 'Jobs Developer', amount: 1150},
      {id: 7, name: '01-Pcl notbook', amount: - 250},
      {id: 8, name: 'Academia', amount: - 100},
-];
+]
+
+const removeValue = ID => {
+     dummyTransaction = dummyTransaction.filter(transaction => transaction.id != ID)
+     init() 
+}
 
 const addTransactionIntoDom = (value) => {
-
      const operator = value.amount < 0 ? '-' : '+'
      const cssClass = value.amount < 0 ? 'minus' : 'plus'
-
      const removeOperatoAmount = Math.abs(value.amount)
-
      const li = document.createElement('li')
-
      li.classList.add(cssClass)
+
      li.innerHTML = `
-          ${value.name}<span>${operator} R$${removeOperatoAmount}</span><button class="delete-btn">x</button>
+          ${value.name}<span>${operator} R$ ${removeOperatoAmount}</span>
+          <button class="delete-btn" onClick="removeValue(${value.id})">
+               x
+          </button>
      `
      transactionsEl.append(li)
 }
 
-/*Pegando valores da chave amount de dentro da array de obj dummyTransaction[],
-com map(), passando o reduce(), para somar todos os valores e passando toFIxed()
-para acrescentar duas casas decimais.
-*/
 const updateBalanceValues = () => {
      const valueAmounts = dummyTransaction
           .map(value => value.amount)
 
      const currentValue = valueAmounts
-          .reduce((accumulator, value) => accumulator + value, 0)
-          .toFixed(2)
-
+          .reduce((accumulator, value) => accumulator + value, 0).toFixed(2)
+     
      const income = valueAmounts
           .filter((value) => value > 0)
-          .reduce((accumulator, value) => accumulator + value, 0)
-          .toFixed(2)
+          .reduce((accumulator, value) => accumulator + value, 0).toFixed(2)
      
-     const expense = valueAmounts
+     const expense = Math.abs(valueAmounts
           .filter((value) => value < 0)
-          .reduce((accumulator, value) => accumulator + value, 0)
+          .reduce((accumulator, value) => accumulator + value, 0))
           .toFixed(2)
 
-     //add os valores nos display da aplicacao
      currentValueDisplayEl.textContent = `R$ ${currentValue}`
      incomeDisplayEl.textContent = `R$ ${income}`
      expenseDisplayEl.textContent = `R$ ${expense}`
-
-     console.log(`
-          Despesas ${expense}, 
-          Receita ${income}, 
-          Saldo atual ${currentValue}
-          `);
 }
 
 const init = () => {
-     dummyTransaction
-          .forEach(addTransactionIntoDom)
-
+     transactionsEl.innerHTML = ''
+     dummyTransaction.forEach(addTransactionIntoDom)
      updateBalanceValues()
 }
 init()
+
+
+const generatorID = () => Math.round(Math.random() * 1000)
+
+formEl.addEventListener('submit', event => {
+     event.preventDefault()
+
+     const imputTextValue = impuTextValueEL.value.trim()
+     const imputAmountValue = imputAmountValueEl.value.trim()
+
+     if(imputTextValue === ''|| imputAmountValue === '') {
+          alert('Por favor inserir os valores nos campos')
+          return
+     }
+
+     const transaction = {
+          id: generatorID(), 
+          name: imputTextValue, 
+          amount: Number(imputAmountValue)
+     }
+     
+     dummyTransaction.push(transaction)
+     init()
+     impuTextValueEL.value = ''
+     imputAmountValueEl.value = ''
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
