49aa89bbea0238b30a3e6f4b40644f671048549e
Adelto Oliveira <adtguitar@gmail.com>
2022-02-07 19:53:03 -0300

    Refatorando o codigo, e criando o metodo updateLocalStorage(), para aramazenarmos os dados na api do google LocalStorage

--- js/app.refatory.js
+++ js/app.refatory.js
@@ -6,19 +6,25 @@ const formEl = document.querySelector('#form')
 const impuTextValueEL = document.querySelector('#text')
 const imputAmountValueEl = document.querySelector('#amount')
 
-let dummyTransaction = [ 
-     {id: 1, name: 'Violao folk', amount: - 900},
-     {id: 2, name: 'Salario', amount: 7500},
-     {id: 3, name: 'Freela', amount: 250},   
-     {id: 4, name: 'Jobs Design', amount: 150},
-     {id: 5, name: 'Mercado', amount: - 150},
-     {id: 6, name: 'Jobs Developer', amount: 150},
-     {id: 7, name: 'notbook', amount: - 1250},
-     {id: 8, name: 'Academia', amount: - 100},
-]
+// let transactions = [ 
+//      {id: 1, name: 'Violao folk', amount: - 900},
+//      {id: 2, name: 'Salario', amount: 7500},
+//      {id: 3, name: 'Freela', amount: 250},   
+//      {id: 4, name: 'Jobs Design', amount: 150},
+//      {id: 5, name: 'Mercado', amount: - 150},
+//      {id: 6, name: 'Jobs Developer', amount: 150},
+//      {id: 7, name: 'notbook', amount: - 1250},
+//      {id: 8, name: 'Academia', amount: - 100},
+// ]
+
+const localStorageTransaction = JSON.parse(localStorage.getItem('transactions'))
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransaction : []
+
 
 const removeValue = ID => {
-     dummyTransaction = dummyTransaction.filter(transaction => transaction.id != ID)
+     transactions = transactions.filter(transaction => transaction.id != ID)
+
+     console.log(transactions);
      init() 
 }
 
@@ -28,6 +34,7 @@ const addTransactionIntoDom = (value) => {
      const cssClass = value.amount < 0 ? 'minus' : 'plus'
      const li = document.createElement('li')
      li.classList.add(cssClass)
+
      li.innerHTML = `
           ${value.name}<span>${operator} R$ ${removeOperatoAmount}</span>
           <button class="delete-btn" onClick="removeValue(${value.id})">
@@ -38,11 +45,12 @@ const addTransactionIntoDom = (value) => {
 }
 
 const updateBalanceValues = () => {
-     const valueAmounts = dummyTransaction.map(value => value.amount)
+     const valueAmounts = transactions.map(value => value.amount)
 
-     const currentValue = valueAmounts.reduce((accumulator, value) => accumulator + value, 0)
+     const currentValue = valueAmounts.reduce((accumulator, value) => accumulator + value, 0).toFixed(2)
      const income = valueAmounts.filter((value) => value > 0).reduce((accumulator, value) => accumulator + value, 0).toFixed(2)
      const expense = Math.abs(valueAmounts.filter((value) => value < 0).reduce((accumulator, value) => accumulator + value, 0)).toFixed(2)
+
 
      currentValueDisplayEl.textContent = `R$ ${currentValue}`
      incomeDisplayEl.textContent = `R$ ${income}`
@@ -51,7 +59,7 @@ const updateBalanceValues = () => {
 
 const init = () => {
      transactionsUl.innerHTML = ''
-     dummyTransaction.forEach(addTransactionIntoDom)
+     transactions.forEach(addTransactionIntoDom)
      updateBalanceValues()
 }
 init()
@@ -68,14 +76,13 @@ formEl.addEventListener('submit', event => {
           alert('Por favor inserir os valores nos campos')
           return
      }
-
-     const transaction = {
+     const transaction = { 
           id: generatorID(), 
           name: imputTextValue, 
           amount: Number(imputAmountValue)
      }
      
-     dummyTransaction.push(transaction)
+     transactions.push(transaction)
      init()
      impuTextValueEL.value = ''
      imputAmountValueEl.value = ''
